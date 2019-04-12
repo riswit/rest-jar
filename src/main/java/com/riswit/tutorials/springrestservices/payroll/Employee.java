5fe4cdc7fc083f0d9af5659c1a69968b2db1bb02
Dino <dino.solano@riswit.it>
2019-04-12 18:18:20 +0200

    added constructor into Employee class

--- src/main/java/com/riswit/tutorials/springrestservices/payroll/Employee.java
+++ src/main/java/com/riswit/tutorials/springrestservices/payroll/Employee.java
@@ -14,6 +14,9 @@ class Employee {
     private String name;
     private String role;
 
+    public Employee() {
+    }
+
     Employee(String name, String role) {
         this.name = name;
         this.role = role;
